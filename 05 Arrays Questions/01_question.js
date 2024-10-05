207d48be7b3ba5196dd181e4e2247bc57d77831a
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-10-06 00:09:56 +0530

    solved arrays question using in-built function in javascript

---
+++ 05 Arrays Questions/01_question.js
@@ -0,0 +1,10 @@
+// Write a program to find the sum of all elements in an array.
+
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for(let i = 0; i < myArr.length; i++){
+    sum += myArr[i]
+}
+
+console.log(sum);
