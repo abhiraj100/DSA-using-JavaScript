585af75d47497cce700cec763b8859af245a060d
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-10-31 18:54:45 +0530

    Solved 16th question of array

---
+++ 05 Arrays Questions/16_question.js
@@ -0,0 +1,8 @@
+// Write a program to shift all elements of an array to the left by one position.
+
+let myArr = [1, 2, 3, 4, 5, 6];
+let temp = myArr[0];
+
+for(let i = 1; i < myArr.length; i++){
+    let temp2 = myArr[]
+}
