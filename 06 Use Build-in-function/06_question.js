60fbee6e8196d4f9466b10d1e6b34cc9e326f24d
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-11-09 16:36:14 +0530

    Solved 6th question of Build-in Function

---
+++ 06 Use Build-in-function/06_question.js
@@ -0,0 +1,6 @@
+// Convert an array of words into a single string, with each word separated by a space.
+
+let arr = ["Codes", "For", "Tomorrow"];
+
+oneWordString = arr.join(" ");
+console.log(oneWordString);
