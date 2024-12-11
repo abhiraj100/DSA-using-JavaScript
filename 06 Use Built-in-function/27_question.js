c0be360564e6df77c2e281d44c77f5471df775d8
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-12-12 02:14:23 +0530

    Solved 27th question using built-in method

---
+++ 06 Use Built-in-function/27_question.js
@@ -0,0 +1,11 @@
+// Join an array of numbers into a single string, with each number separated by a comma.
+
+let arr = [1, 2, 3, 4, 5];
+
+// let arrayToSingleString = arr.toString();
+// console.log(arrayToSingleString);
+
+// Approach 2
+let arrayToSingleString = arr.join(", ");
+console.log(arrayToSingleString);
+
