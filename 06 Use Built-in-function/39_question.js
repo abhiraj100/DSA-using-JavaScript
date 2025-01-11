2a61b95a717176a20cfd4b8716a6afa4f639349a
Abhiraj Yadav <abhiraj100right@gmail.com>
2025-01-12 00:52:48 +0530

    Solved 39th question using built-in method

---
+++ 06 Use Built-in-function/39_question.js
@@ -0,0 +1,27 @@
+// Sort an array of numbers in descending order and return the sorted array.
+
+// // Approach 1
+
+// let arr = [5, 2, 6, 3, 9, 1, 15];
+
+// let newArr = arr.sort((a, b) => b - a);
+// console.log(newArr)
+
+// // Approach 2
+
+// let arr = [5, 2, 6, 3, 9, 1, 15];
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length - 1; j++){
+//         if(arr[j] < arr[j + 1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+// Approach 3
+
+
