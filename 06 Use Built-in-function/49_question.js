e5fe291ccf1bf05318766e3bd80f32220e70c5cd
Abhiraj Yadav <abhiraj100right@gmail.com>
2025-01-22 02:35:53 +0530

    Solved 49th question using built-in method

---
+++ 06 Use Built-in-function/49_question.js
@@ -0,0 +1,18 @@
+// Determine the last index of a given object in an array of objects based on a specific property.
+
+let arr = [
+    {
+        name : "abhiraj",
+        age : 22,
+    },
+    {
+        name : "abhiraj",
+        age : 22,
+    }
+]
+
+let newArr = arr.map((el) => {
+    return {
+        el.lastIndex
+    }
+})
